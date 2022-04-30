458bab85994654e6dc8c30ba9a36cbe4229ea627
Dave Farinelli <d@dfar.io>
2022-04-30 20:37:58 +0000

    Fills out guest information

--- thai_taste_to_go.ts
+++ thai_taste_to_go.ts
@@ -34,26 +34,39 @@ var dinnerOrders = [
 
     // exit popup
     await page.click('button.close.close-button');
-    await page.waitForTimeout(1000);
 
     // check which menu is available
     const isLunchMenu = await page.evaluate(el => el.classList.contains('theme'), await page.$('label[for="tab1"]'));
     console.log(`isLunchMenu: ${isLunchMenu}`);
+
+    // select order
     const order = isLunchMenu ? 
-      lunchOrders[Math.floor(Math.random()*lunchOrders.length)] :
-      dinnerOrders[Math.floor(Math.random()*dinnerOrders.length)];
+      lunchOrders[Math.floor(Math.random() * lunchOrders.length)] :
+      dinnerOrders[Math.floor(Math.random() * dinnerOrders.length)];
     console.log(`order: ${order.name}`);
 
     // click food entry
     await page.click(`css=span.foodName-text >> text="${order.name}"`);
-    await page.waitForTimeout(1000);
 
     // select options - needs to include "special" notes for order
     const chickenValue = "Srd8o2evE8g=";
     const mediumPlusValue = "D4g2pzByV40=";
     await page.click(`input[value="${chickenValue}"] >> visible=true`);
     await page.click(`input[value="${mediumPlusValue}"] >> visible=true`);
-    await page.waitForTimeout(1000);
+    await page.fill('textarea', order.special);
+    await page.click('button:has-text("Add to cart") >> nth=0');
+
+    // click checkout, then checkout as quest
+    await page.click(`button.theme-btn-checkout >> nth=2`);
+    await page.click('div.button >> nth=1');
+
+    // fill out guest information
+    await page.fill('#txtGuestFirstName', 'Dave');
+    await page.fill('#txtGuestLastName', 'Farinelli');
+    await page.fill('#txtGuestEmail', 'a@a.com');
+    await page.fill('#txtGuestPhone', '(123) 456-7890');
+    await page.click(`#btn-next-user`);
+    await page.waitForTimeout(5000);
   } catch (err) {
     await context.close();
     throw err;
