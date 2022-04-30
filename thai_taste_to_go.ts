f4f47ef4e7daf40db3666466c6b79adfb83d1d50
Dave Farinelli <d@dfar.io>
2022-04-30 17:12:38 +0000

    Works with lunch/dinner

--- thai_taste_to_go.ts
+++ thai_taste_to_go.ts
@@ -2,7 +2,7 @@
 
 const { webkit } = require('playwright');
 
-var orders = [
+var lunchOrders = [
 {
   name: 'LN1 Pad Thai',
   special: 'no peanuts'
@@ -11,7 +11,16 @@ var orders = [
   name: 'LK6 Kow Pad Kra Tiem',
   special: 'no onions'
 }];
-var order = orders[Math.floor(Math.random()*orders.length)];
+
+var dinnerOrders = [
+{
+  name: 'N1 Pad Thai',
+  special: 'no peanuts'
+},
+{
+  name: 'K6 Kow Pad Kra Tiem',
+  special: 'no onions'
+}];
 
 (async () => {
   const browser = await webkit.launch();
@@ -21,6 +30,14 @@ var order = orders[Math.floor(Math.random()*orders.length)];
   // exit popup
   await page.click('button.close.close-button');
   await page.waitForTimeout(1000);
+
+  // check which menu is available
+  const isLunchMenu = await page.evaluate(el => el.classList.contains('theme'), await page.$('label[for="tab1"]'));
+  console.log(`isLunchMenu: ${isLunchMenu}`);
+  const order = isLunchMenu ? 
+    lunchOrders[Math.floor(Math.random()*lunchOrders.length)] :
+    dinnerOrders[Math.floor(Math.random()*dinnerOrders.length)];
+  console.log(`order: ${order.name}`);
 
   // click food entry
   await page.click(`css=span.foodName-text >> text="${order.name}"`);
