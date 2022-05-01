7904f2b05e88bd2015b79e301fc1ca53859d3d1b
Dave Farinelli <d@dfar.io>
2022-05-01 01:28:35 +0000

    Adds env vars, almost complete?

--- thai_taste_to_go.ts
+++ thai_taste_to_go.ts
@@ -23,6 +23,7 @@ var dinnerOrders = [
 }];
 
 (async () => {
+  const isLiveRun = process.argv[2] && process.argv[2] === '-live';
   const browser = await webkit.launch();
   const context = await browser.newContext({
     recordVideo: { dir: 'video' }
@@ -30,6 +31,7 @@ var dinnerOrders = [
   const page = await context.newPage();
 
   try {
+    const env = getEnvVars();
     await page.goto('https://thaitastetogomi.smiledining.com/');
 
     // exit popup
@@ -61,12 +63,30 @@ var dinnerOrders = [
     await page.click('div.button >> nth=1');
 
     // fill out guest information
-    await page.fill('#txtGuestFirstName', 'Dave');
-    await page.fill('#txtGuestLastName', 'Farinelli');
+    await page.fill('#txtGuestFirstName', env.FIRST_NAME);
+    await page.fill('#txtGuestLastName', env.LAST_NAME);
     await page.fill('#txtGuestEmail', 'a@a.com');
     await page.fill('#txtGuestPhone', '(123) 456-7890');
     await page.click(`#btn-next-user`);
-    await page.waitForTimeout(5000);
+
+    // progress to CC info
+    await page.click(`button.theme-btn-checkout >> nth=2`);
+
+    // fill out CC info
+    await page.fill('.txtCCNumber', env.CC_NUMBER);
+    await page.fill('#txtCCName', `${env.FIRST_NAME} ${env.LAST_NAME}`);
+    await page.fill('#txtCCExpiry', env.CC_EXPIRY);
+    await page.fill('#txtCCCVC', env.CC_CVC);
+    await page.fill('#txtBillingAddress', env.ADDRESS);
+    await page.fill('#txtBillingZip', env.ZIP);
+    await page.click('button[value="10"]');
+
+    // click confirm if non-dry run.
+    if (isLiveRun) {
+      await page.click('button.swal2-confirm');
+    } else {
+      console.log('Dry run: order not submitted.');
+    }
   } catch (err) {
     await context.close();
     throw err;
@@ -75,3 +95,29 @@ var dinnerOrders = [
   await context.close();
   await browser.close();
 })();
+
+function getEnvVars() {
+  const envVars = {
+    FIRST_NAME: process.env.FIRST_NAME,
+    LAST_NAME: process.env.LAST_NAME,
+    CC_NUMBER: process.env.CC_NUMBER,
+    CC_EXPIRY: process.env.CC_EXPIRY,
+    CC_CVC: process.env.CC_CVC,
+    ADDRESS: process.env.ADDRESS,
+    ZIP: process.env.ZIP
+  };
+    
+  verifyEnvVars(envVars)
+    
+  return envVars;
+}
+
+function verifyEnvVars(env_vars) {
+  for (const property in env_vars) {
+    const value = env_vars[property];
+    if (value === undefined || value === '') {
+      console.error(`Environment variable ${property} not defined.`)
+      process.exit(1);
+    }
+  }
+}
